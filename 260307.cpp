04a53a0e9c9912cfed8c385be3157fc3df17488a
mini73 <ikm020526@gmail.com>
2026-03-07 18:00:57 +0900

    Add files via upload

---
+++ 260307.cpp
@@ -0,0 +1,207 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("사과 1개\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 1;
+//	printf("사과 %d개\n", apple);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("키보드 3개\n");
+//	printf("마우스 2개\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int keyboard = 3;
+//	int mouse = 2;
+//	printf("키보드: %d\n", keyboard);
+//	printf("마우스: %d\n", mouse);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int coin1 = 500;
+//	int coin2 = 1000;
+//	printf("%d + %d = %d\n",
+//		coin1, coin2, coin1 + coin2);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char A;
+//	A = 'A';
+//	printf("A = %c, A = %d\n", A, A);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double pi;
+//	pi = 3.14;
+//	float e = 2.718;
+//	printf("pi = %.2lf, e = %.3f.. \n", pi, e);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    double pi;
+//	pi = 3.14;
+//	int r = 5;
+//	double area = 0;
+//	area = r * r * pi;
+//	double cir = 0;
+//	cir = 2 * r * pi;
+//	printf("원의 넓이는 %lf,원의 둘레는 %lf입니다\n", area, cir);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int us = 3;
+//	int ls = 5;
+//	int h = 7;
+//	int area = 0;
+//	area = (us + ls)* h / 2;
+//	printf("사다리꼴의 넓이는 %d이다\n", area);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int k;
+//	printf("원하는 숫자를 입력 : ");
+//	scanf_s("%d", &k);
+//	printf("입력한 숫자는 : %d\n", k);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int y, x;
+//	printf("더하고 싶은 값 2개 입력 : ");
+//	scanf_s("%d %d", &y, &x);
+//	printf("두 값의 합은 %d입니다.\n", y + x);
+//	return 0;
+////}
+//#include <stdio.h>
+//int main()
+//{
+//	char a, b, c;
+//	printf("문자 두 개 입력 : ");
+//	scanf("%c%c%c", &a, &b, &c);
+//	printf("문자출력 : %c%c%c\n", a, b, c);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	double pi, area;
+//	pi = 3.14;
+//	area = 0;
+//	int length;
+//	length = 0;
+//	int r;
+//	printf("원의 반지름을 입력 : ");
+//	scanf("%d", &r);
+//	printf("원의 넓이는 %lf, 원의 길이는 %d입니다.", r * r * pi, r + r);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	double W1, W2, H;
+//	double area = 0;
+//	printf("밑변의 길이,윗변의 길이, 높이를 입력하시오: ");
+//	scanf("%lf %lf %lf", &W1, &W2, &H);
+//	printf("사다리꼴의 넓이는 %.2lf입니다", (W1 + W2) * H / 2);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 15;
+//	int b = 3;
+//	printf("%d + %d = %d\n", a, b, a + b);
+//	printf("%d - %d = %d\n", a, b, a - b);
+//	printf("%d * %d = %d\n", a, b, a * b);
+//	printf("%d / %d = %d\n", a, b, a / b);
+//	printf("%d %% %d = %d\n", a, b, a % b);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main() 
+//{
+//	int number = 1;
+//	number = number + 3; printf("%2d\n", number);
+//	number -= 2;         printf("%2d\n", number);
+//	number--;            printf("%2d\n", number);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", a++);
+//	printf("%d\n", a);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", ++a);
+//	printf("%d\n", a);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	printf("숫자 2개 입력: ");
+//	scanf("%d %d", &a, &b);
+//
+//	printf("%d + %d = %d\n", a, b, a + b);
+//	printf("%d - %d = %d\n", a, b, a - b);
+//	printf("%d * %d = %d\n", a, b, a * b);
+//	printf("%d / %d = %d\n", a, b, a / b);
+//	printf("%d %% %d = %d\n", a, b, a % b);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int first = 1 % 4;
+//	printf("%d = 1%%4\n", first);
+//	int second = 5 % 3;
+//	printf("%d = 5%%3\n", second);
+//	int third = 4 % 2;
+//	printf("%d = 4%%2\n", third);
+//	return 0;
+//}
+#include<stdio.h>
+
+int main
