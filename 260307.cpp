fc7ef66d649b4fe50ef9ca15b14710fc7ad5b96d
mini73 <ikm020526@gmail.com>
2026-03-09 19:54:18 +0900

    Add files via upload

--- 260307.cpp
+++ 260307.cpp
@@ -202,6 +202,172 @@
 //	printf("%d = 4%%2\n", third);
 //	return 0;
 //}
-#include<stdio.h>
-
-int main
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int number1, number2;
+//	scanf("%d %d", &number1, &number2);
+//	printf("%d = number1%%4\n", number1 % 4);
+//	printf("%d = number2%%3\n", number2 % 3);
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int first = 1 % 3;
+//	printf("%d = 1%%3\n", first);
+//	int second = 2 % 3;
+//	printf("%d = 2%%3\n", second);
+//	int third = 3 % 3;
+//	printf("%d = 3%%3\n", third);
+//	int fourth = 4 % 3;
+//	printf("%d = 4%%3\n", fourth);
+//	int fifth = 5 % 3;
+//	printf("%d = 5%%3\n", fifth);
+//	int sixth = 6 % 3;
+//	printf("%d = 6%%3\n", sixth);
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int number1;
+//	scanf("%d", &number1);
+//	int number2 = number1 % 50;
+//	printf("답은 %d입니다", number2);
+//	return 0;
+//
+//}
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int data1 = 6 / 2;
+//	printf("%d =6/2\n", data1);
+//	int data2 = 6 / 3;
+//	printf("%d =6/3\n", data2);
+//	int data3 = 6 / 4;
+//	printf("%d =6/4\n", data3);
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int number1, number2;
+//	scanf("%d %d", &number1, &number2);
+//	printf("%d=", number1 / number2);
+//	printf("%d/%d\n", number1, number2);
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int coin;
+//	printf("돈을 입력해주세요.(원)");
+//	scanf("%d", &coin);
+//
+//	int coin10000 = coin / 10000;
+//	coin = coin % 10000;
+//	int coin5000 = coin / 5000;
+//	coin = coin % 5000;
+//	int coin1000 = coin / 1000;
+//	printf("만원권 %d개, ", coin10000);
+//	printf("오천원권 %d개, ", coin5000);
+//	printf("천원권 %d개, ", coin1000);
+//	printf("나머지는 동전입니다.");
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int jel;
+//	printf("젤리가격을 입력해주세요.(원)");
+//	scanf("%d", &jel);
+//
+//	/*int coin500 = jel ;
+//	jel = jel % 500;
+//	int coin100 = 100 / jel;
+//	jel = jel % 100;
+//	int coin50 = 50 / jel;
+//	jel = jel % 50;
+//	int coin10 = 10 / jel;
+//	jel = jel % 10;
+//	printf("오백원 %1d개, ", coin500);
+//	printf("백원 %1d개, ", coin100);
+//	printf("오십원 %1d개, ", coin50);
+//	printf("십원 %1d개, ", coin10);*/
+//
+//	int change = 1000 - jel;
+//
+//	int coin500 = change / 500;
+//	change %= 500; //반복되는 구간은 축약식으로 써주면 좋다
+//	int coin100 = change / 100;
+//	change = change % 100;
+//	int coin50 = change / 50;
+//	change = change % 50;
+//	int coin10 = change / 10;
+//	
+//	printf("오백원 %d개, ", coin500);
+//	printf("백원 %d개, ", coin100);
+//	printf("오십원 %d개, ", coin50);
+//	printf("십원 %d개, ", coin10);
+//
+//	return 0;
+//}
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	int randInt = rand() % 10;
+//	int nextInt = rand() % 10;
+//	printf("첫숫자 : %d\n", randInt);
+//	printf("다음숫자 : %d", nextInt);
+//
+//	return 0;
+// }
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//
+//int main()
+//{
+//	srand((unsigned int)time(NULL));
+//	int randInt = rand() % 10;
+//	int nextInt = rand() % 10;
+//	int afterInt = rand() % 10 + 50;
+//	printf("첫숫자 : %d\n", randInt);
+//	printf("다음숫자 :%d\n", nextInt);//0~9
+//	printf("그다음숫자 :%d", afterInt);//50~59
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//
+//int main()
+//{
+//	srand((unsigned int)time(NULL));
+//	printf("내 주머니에 있는 돈은");
+//	int myMoney = (rand() % 10 + 1) * 1000; //1000~10000
+//	printf("%d원이다.\n", myMoney);
+//	int coin500 = rand() % 4 * 500;//0~1500
+//	int coin100 = rand() % 5000;//0~4999
+//	coin100 = coin100 / 100 * 100;//0~4900
+//	printf("그리고 과자의 가격은");
+//	printf("%d원이다.", coin500 + coin100);
+//	printf("\n나는 과자를 살 수 있을까?");
+//
+//	return 0;
+//}
